3b052e70fdd668233b8b74acbc4da7a7552ca3f8
end1123 <50690780+end1123@users.noreply.github.com>
2021-09-25 10:54:34 +0800

    Add files via upload

---
+++ ysys.js
@@ -0,0 +1,39 @@
+var tvPlaysList = [
+	{
+		name : "一生一世",
+		image : "https://pic0.iqiyipic.com/image/20210914/9f/30/a_100447497_m_601_m4_260_360.jpg",
+		size: 30,
+		path: [
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_1d0dmvryo2o.html",//1
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_11eobqnbzvs.html",//2
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_29g2yq23j68.html",//3
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_20ltcj4qgng.html",//4
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_lqf5y4suas.html",//5
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_2biixysyh40.html",//6
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_29xxuifor50.html",//7
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_ugzxk67868.html",//8
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_ds3kar0ur4.html",//9
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_1rczkx9agaw.html",//10
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_nxvzig12lo.html",//11
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_sxx1hqk7ek.html",//12
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_vezj96gc5s.html",//13
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_27u2uhqr73k.html",//14
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_vci9gksp6s.html",//15
+			"https://vip.parwix.com:4433/player/?url=http://www.iqiyi.com/v_1fxheaf2vi0.html",//16
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_t3jo6pi3m4.m3u8",//17
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_17jw22rflvc.m3u8",//18
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_10wg1d3g3xc.m3u8",//19
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_1hzgjyxe5q0.m3u8",//20
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_qzoplem8z4.m3u8",//21
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_n2bwi5sggk.m3u8",//22
+            "https://vip.parwix.com:4433/player/?url=https://www.iqiyi.com/v_mdmbojar0k.html",//23
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_1vm3gobvlws.m3u8",//24
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_1qa285zcur8.m3u8",//25
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_jiikd6jfao.m3u8",//26
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_naots1gxlw.m3u8",//27
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_umsfqyw9w0.m3u8",//28
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_1813fjg8q80.m3u8",//29
+            "https://jx.mmkv.cn/m3u8.php?url=https://vip.qianyicp.com/data/api_m3u8_j/iqiyi_20ow6zjoux4.m3u8",//30
+		]
+	}
+];
